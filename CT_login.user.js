eb5e3ef97f3d6eb0537b2ea18d36aeb6106d0d69
James Clare <bluecombats@users.noreply.github.com>
2021-11-18 09:49:22 +0000

    Create CT_login.user.js

---
+++ CT_login.user.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name        CT_login
+// @namespace   https://github.com/bluecombats/CentralTickets/CT_login.user.js
+// @description turn on automcomplete for email and password
+// @include     https://centraltickets.co.uk/
+// @include     *centraltickets.co.uk/*
+// @version     1.2021.11.18.0929
+// @grant       none
+// @run-at 	document-end
+// @copyright   2015, cameroncondry
+// ==/UserScript==
+
+function CT_login(){
+	var CT_loginInterval = setInterval(function(){
+		InsertLogin();
+	}, 1000);
+}
+
+function InsertLogin(){
+  input = document.getElementsByTagName("input");
+  if(input.getElementById("email_addr")[0]){
+    var email = document.getElementsByClassName("login-popup button_tooltip tooltip-block")[0];
+    //console.log(email);
+    //https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/password
+    email.setAttribute("automComplete","on");
+  }
+  for(i=0;i<=input.length;i++){
+    if(input[i].type == "password"){
+      input[i].setAttribute("automComplete","on");
+      break;
+    }
+  }
+}
+//Main script starts here
+initCT_login();
